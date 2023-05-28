15750a662c2a0096831c3712b7100c08f9e26542
jdhl27 <independientemedellin.27.03@gmail.com>
2023-05-27 21:41:07 -0500

    Add request to request user data before the app is mounted

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import { useUserStore } from "./store/userFire";
 
-const pinia = createPinia()
-const app = createApp(App)
+const pinia = createPinia();
+const app = createApp(App);
 
-app.use(pinia)
-app.use(router)
-app.mount('#app')
+app.use(pinia);
+
+const usuariosS = useUserStore();
+
+app.mixin({
+  beforeMount() {
+    usuariosS.currentUser();
+  },
+});
+
+app.use(router);
+app.mount("#app");
