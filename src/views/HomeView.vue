fb947214bec42105ed990abcc11a939414dfcf35
jdhl27 <independientemedellin.27.03@gmail.com>
2023-05-28 17:37:28 -0500

    Modify home structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,12 +11,14 @@ import Footer from "../components/Footer.vue";
       />
     </div>
     <div class="row">
-      <div class="col-md-6">
+      <div
+        class="col-md-6 d-flex d-flex justify-content-center align-items-center"
+      >
         <div class="my-5">
           <h1 class="display-4">Ahorra con nosotros</h1>
           <p class="lead">
             Ofrecemos las mejores tasas de interés del mercado en nuestras
-            cuentas de ahorro.
+            cuentas de ahorro y un ahorro inicial de $100.
           </p>
           <hr class="my-4" />
           <p>¡Comenzá a ahorrar hoy mismo con nuestro banco!</p>
@@ -27,26 +29,12 @@ import Footer from "../components/Footer.vue";
             >Abrir cuenta</router-link
           >
         </div>
-        <div class="my-5">
-          <h1 class="display-4">Obtené un crédito</h1>
-          <p class="lead">
-            Te ofrecemos los mejores planes de financiamiento para cumplir tus
-            sueños.
-          </p>
-          <hr class="my-4" />
-          <p>¡Solicitá un crédito con nosotros hoy mismo!</p>
-          <router-link
-            class="btn btn-primary btn-lg"
-            to="/account"
-            role="button"
-            >Solicitar crédito</router-link
-          >
-        </div>
       </div>
       <div class="col-md-6 d-flex justify-content-center">
+        <div class="home-hero-circle"></div>
         <img
-          style="width: 100%"
-          src="src/assets/hero.png"
+          class="animated-image"
+          src="src/assets/hero2.png"
           alt="Imagen atractiva"
         />
       </div>
@@ -62,13 +50,13 @@ import Footer from "../components/Footer.vue";
 }
 
 .col-md-6:nth-child(1) h1 {
-  font-size: 48px;
-  color: #000;
+  font-size: 62px;
+  color: #0e1523;
 }
 
 .col-md-6:nth-child(1) p {
-  font-size: 24px;
-  color: #666;
+  font-size: 18px;
+  color: rgba(14, 21, 35, 0.61);
 }
 
 .col-md-6:nth-child(1) hr {
@@ -76,15 +64,18 @@ import Footer from "../components/Footer.vue";
 }
 
 .col-md-6:nth-child(1) a.btn-primary {
-  font-size: 24px;
-  font-weight: bold;
+  font-size: 20px;
   border-color: #333;
-  background-color: #333;
+  padding: 15px 41px;
+  background-color: #0e1523;
+  transition: box-shadow 0.3s, transform 0.3s;
 }
 
 .col-md-6:nth-child(1) a.btn-primary:hover {
   background-color: #000;
   border-color: #000;
+  box-shadow: 0 4px 30px rgba(20, 30, 50, 0.35);
+  transform: translate3d(0, -3px, 0.01px);
 }
 .bg-2.home-hero-bg {
   z-index: -1;
@@ -100,5 +91,31 @@ import Footer from "../components/Footer.vue";
 
 .bg-2.home-hero-bg-shape {
   max-width: 67%;
+}
+
+.home-hero-circle {
+  z-index: -1;
+  position: absolute;
+  width: 36%;
+  min-height: 589px;
+  background-image: linear-gradient(152.59deg, #141e32, #0e1523);
+  border-radius: 9999px;
+  top: 242px;
+}
+
+.animated-image {
+  width: 100%;
+  animation: floatAnimation 3s ease-in-out infinite;
+  filter: drop-shadow(0 2px 4px #fff);
+}
+
+@keyframes floatAnimation {
+  0%,
+  100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-10px);
+  }
 }
 </style>
