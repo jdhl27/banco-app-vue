0db057cd00c265f7d1eb12dc6d869b29ed8539d1
jdhl27 <independientemedellin.27.03@gmail.com>
2023-05-29 23:02:00 -0500

    Method to format the date

--- src/store/userFire.js
+++ src/store/userFire.js
@@ -27,6 +27,14 @@ export const useUserStore = defineStore("userStore", {
       return cardNumber;
     },
 
+    getCurrentDate() {
+      const now = new Date();
+      const year = now.getFullYear();
+      const month = String(now.getMonth() + 1).padStart(2, "0");
+      const day = String(now.getDate()).padStart(2, "0");
+      return `${year}-${month}-${day}`;
+    },
+
     async forgotPassword(email) {
       return new Promise((resolve, reject) => {
         sendPasswordResetEmail(auth, email)
